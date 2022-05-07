8dc7158f7704a35d2ce4a9f90203a792a60d3848
ukjunior <devmagnusuche@gmail.com>
2022-05-07 03:20:30 -0700

    setup interactive rating component

---
+++ interactive-rating-component-main/login.js
@@ -0,0 +1,32 @@
+const btns = document.getElementById("bnt");
+const displayNo = document.getElementById("selectedRate");
+const rateno = document.querySelectorAll(".rating-no");
+const card1 = document.querySelector(".card_1");
+const card2 = document.querySelector(".card_2");
+
+
+    btns.addEventListener('click', onsubmit);
+    rateno.forEach(btn => {
+        btn.addEventListener('click', handleRatingClick)
+    });
+
+
+function onsubmit(){
+    card1.classList.add('hide');
+    card2.classList.remove('hide');
+}
+
+
+function handleRatingClick(event) {
+    displayNo.innerHTML = event.target.innerHTML
+rateno.forEach(btn => {
+  btn.classList.remove('active');
+})
+
+if(event.target.classList.contains("rating-no")){
+    event.target.classList.add("active");
+} 
+else{
+    event.target.classList.remove("active");
+}
+}
